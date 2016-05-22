40f33f31741b24c294db6ab86d88345390c96caf
jagn <cursosjagn@gmail.com>
2016-05-22 15:48:46 -0500

    Configuracion de colletions Biblioteca ...

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -10,39 +10,16 @@ export const Libros = new Mongo.Collection('libros);
 
 Meteor.methods({
 
-	'tasks.insert'(text){
-
-		check(text, String);
-
-		if(! this.userId){
-			throw new meteor.Error("Not-autorized");
-		}
-
-		Tasks.insert({
-	      text,
-	      createdAt: new Date(), // current time
-	      owner: Meteor.userId(),
-	      username: Meteor.user.findOne(this.userId).username,
-    	});
-
-	},
-
-	'Tasks.remove'(taskId){
-	      check(taskId, String);
-	      check.remove(taskId);
-    	},
-
-    'Tasks.setChecked'(taskId, setChecked){
-    		check(taskId, String);
-    		check(setChecked, Boolean);
-
-    		Tasks.update(taskId, {$set: { ckecked : setChecked}});
-    	},
-
+	
 });
 
 if (Meteor.isServer){
 	Meteor.publish('tasks' , function tasksPublication(){
-		return Tasks.find();
+		return Estudiantes.find();
+	});
+}
+if (Meteor.isServer){
+	Meteor.publish('tasks' , function tasksPublication(){
+		return Libros.find();
 	});
 }
