8b7ab45eaabf643bd8d73a2dfa67d8b4803c44cc
jagn <cursosjagn@gmail.com>
2016-05-22 15:24:17 -0500

    Configuracion Biblioteca Body.Helpers..

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -8,8 +8,62 @@ import './temLibro.js';
 import './body.html';
 
 
+Template.body.helpers({
+  estudiantes() {
+  	const instance = Template.instance();
+    if (instance.state.get('')) {
+      // If hide completed is checked, filter tasks
+      return Estudiantes.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+
+    return Estudiantes.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
+});
+
+Template.body.helpers({
+  estudiantes() {
+  	const instance = Template.instance();
+    if (instance.state.get('estudiantes')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
+});
+
+
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const text = event.target.text.value;
+ 
+    // Insert a task into the collection
+    Meteor.call('tasks.insert', text);
+ 
+    // Clear form
+    event.target.text.value = '';
+  },
+
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  },
+});
+
+
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
 });
 
 
